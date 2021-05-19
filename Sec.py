7545d7f2b4bc3413617247ab85c16368a2c74c17
SaiNarra8 <76062271+SaiNarra8@users.noreply.github.com>
2021-05-19 14:23:09 +0530

    Update Sec.py

--- Sec.py
+++ Sec.py
@@ -1,12 +1,67 @@
 import csv
+import heapq
 import sys
 
+
+
+'''EFFICIENT APPROACH USING HEAPS'''
+
+
+print('\n\nSOLUTION USING HEAPS\n')
+
+with open(sys.argv[1],'r') as marks:
+    c_reader = csv.reader(marks)
+
+    next(c_reader)
+
+
+
+    total = []
+    mheap = []
+    bheap = []
+    eheap = []
+    pheap = []
+    cheap = []
+    hheap = []
+
+    for line in c_reader:
+        ltotal = 0
+        for i in range(1,7):
+            ltotal += int(line[i])
+        heapq.heappush(total,(ltotal,line[0]))
+
+        heapq.heappush(mheap,(int(line[1]),line[0]))
+        heapq.heappush(bheap,(int(line[2]),line[0]))
+        heapq.heappush(eheap,(int(line[3]),line[0]))
+        heapq.heappush(pheap,(int(line[4]),line[0]))
+        heapq.heappush(cheap,(int(line[5]),line[0]))
+        heapq.heappush(hheap,(int(line[6]),line[0]))
+
+    print('\nTopper in Math is: ',heapq.nlargest(1,mheap)[0][1])        
+    print('Topper in Biology is: ',heapq.nlargest(1,bheap)[0][1])  
+    print('Topper in English is: ',heapq.nlargest(1,eheap)[0][1])  
+    print('Topper in Physics is: ',heapq.nlargest(1,pheap)[0][1])  
+    print('Topper in Chemistry is: ',heapq.nlargest(1,cheap)[0][1])  
+    print('Topper in Hindi is: ',heapq.nlargest(1,hheap)[0][1]) 
+    print('\n\nBest students in the class are : ',heapq.nlargest(3,total))  
+
+
+'''Big O Notation for finding individual subject toppers is O(nlogn) 
+   So in each line we're inserting 6 subject marks to 6 heaps so 6logn which is going to be iterated n times so O(6nlogn) = O(nlogn)'''
+
+'''Big O Notation for finding Top Three Students is O(nlogn)
+   We're inserting ltotal to one heap which takes O(logn) and that is repeated n times so O(nlogn)'''
+
+
+'''BRUTE FORCE APPROACH'''
+
+print('\n\nSOLUTION USING THREE ITERATIONS\n')
 with open(sys.argv[len(sys.argv)-1],'r') as marklist:
     csv_reader = csv.reader(marklist)
     next(csv_reader)
     
     
-   
+
     mtopper = {'name':None,'marks':0}
     btopper = {'name':None,'marks':0}
     etopper = {'name':None,'marks':0}
@@ -20,11 +75,11 @@ with open(sys.argv[len(sys.argv)-1],'r') as marklist:
                'name3':None,'marks3':0
               }
     
+
     for line in csv_reader:
         ltotal = 0
         for i in range(1,7):
             ltotal += int(line[i])
-        
 
 
         if(ltotal>toppers['marks1']):
@@ -88,3 +143,4 @@ with open(sys.argv[len(sys.argv)-1],'r') as marklist:
 
     '''Big O Notation for finding individual topper is O(n)
        So it takes 3 iterations for Top 3 students so O(n) + O(n) + O(n) which is equal to O(n) so LINEAR TIME COMPLEXITY'''
+
